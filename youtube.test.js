75235749cecfb39f03cbc61ca7f4364bdfb62ed7
amelia9981 <67780974+amelia9981@users.noreply.github.com>
2021-06-20 06:47:16 +0900

    Add test

---
+++ youtube.test.js
@@ -0,0 +1,24 @@
+import {chrome} from 'jest-chrome'
+
+test('message sending check', () => {
+    const listenerSpy = jest.fn()
+    const sendResponseSpy = jest.fn()
+
+    chrome.runtime.onMessage.addListener(listenerSpy)
+
+    expect(listenerSpy).not.toBeCalled()
+    expect(chrome.runtime.onMessage.hasListeners()).toBe(true)
+
+    chrome.runtime.onMessage.callListeners(
+        { greeting: 'hello' }, // message
+        {}, // MessageSender object
+        sendResponseSpy, // SendResponse function
+    )
+
+    expect(listenerSpy).toBeCalledWith(
+        { greeting: 'hello' },
+        {},
+        sendResponseSpy,
+    )
+    expect(sendResponseSpy).not.toBeCalled()
+})
