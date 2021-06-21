51e99765ac87c3b0d8b62d410d483294bdf4751e
han <consistant1y@ajou.ac.kr>
2021-06-21 17:16:17 +0900

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,4 @@ module.exports = {
     testEnvironment: 'node',
     setupFilesAfterEnv: ['./jest.setup.js'],    
     moduleDirectories: ["node_modules"],
-    reporters: [
-        'default', [
-            './node_modules/jest-html-reporter',
-            {
-                pageTitle: 'Test Report',
-                outputPath: 'output/test/test-report.html',
-            },
-        ],
-    ],
 };
